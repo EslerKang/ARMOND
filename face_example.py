9b839f52c986cc99c8900d1f254a60df51737c31
eslerkang <eslerkang@gmail.com>
2019-12-31 13:38:10 +0900

    new image and files for border exmaple added

--- face_example.py
+++ face_example.py
@@ -1,11 +1,12 @@
 import cv2
 
-#웹캠에서 영상을 읽어온다
 cap = cv2.VideoCapture(0)
-cap.set(3, 640) #WIDTH
-cap.set(4, 480) #HEIGHT
+cap.set(3, 640)
+#WIDTH
 
-#얼굴 인식 캐스케이드 파일 읽는다
+cap.set(4, 480)
+#HEIGHT
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
 
 while(True):
@@ -15,15 +16,15 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    #인식된 얼굴 갯수를 출력
+    #count faces
     if cv2.waitKey(1) & 0xFF == ord('c'):
         print(len(faces))
 
-    # 인식된 얼굴에 사각형을 출력한다
+    #make rectangular shape on the face
     for (x,y,w,h) in faces:
          cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
 
-    #화면에 출력한다
+    #output
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('w'):
         break
