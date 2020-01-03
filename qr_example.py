05c3354b6fcaf9d7fb680132d31a4596798f18bd
eslerkang <eslerkang@gmail.com>
2020-01-03 14:54:52 +0900

    beautified

--- qr_example.py
+++ qr_example.py
@@ -1,9 +1,13 @@
+# a.0.1
+# code by EslerKang
+
 import cv2
-import numpy as np
+# import numpy as np
 import pyzbar.pyzbar as pyzbar
 
+CAM_NUM = 0
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(CAM_NUM)
 font = cv2.FONT_HERSHEY_PLAIN
 
 # 너비
@@ -26,11 +30,12 @@ if cap.isOpened():
             # QR 코드값 화면에 출력
             cv2.putText(frame, (str(obj.data).split('_'))[len(str(obj.data).split('_'))-1], (50, 50), font, 2,
                         (255, 0, 0), 3)
+
             # QR 코드값 검사 후 표시(추후 서버로 다이렉팅 혹은 데이터베이스 활용(?))/Upper, Front 색 다르게 매칭
             if str(obj.data).find("armond_project_") >= 0:
                 # QR 코드 둘레에 사격형 표시
 
-                # Upper, Front와 색상 쌍
+                # Upper, Front, 색상 쌍
                 up_fro = {'_upper_': (255, 0, 0), '_front_': (0, 255, 0)}
 
                 for i in range(0, 3):
@@ -42,7 +47,8 @@ if cap.isOpened():
                                 cv2.line(frame, (obj.polygon[0].x, obj.polygon[0].y), (obj.polygon[3].x,
                                                                                        obj.polygon[3].y), up_fro[j], 3)
 
-        cv2.imshow('frame', frame)
+        cv2.imshow('Armond_project_alpha', frame)
+
         # w 키 입력시 종료
         if cv2.waitKey(1) & 0xFF == 27:
             break
